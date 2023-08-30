28735868891379302b68d503c738c364ed7d1675
Pablo Araya <arayap812@gmail.com>
2023-08-30 20:02:04 -0300

    add icon and format code

--- src/components/meteor-preview.tsx
+++ src/components/meteor-preview.tsx
@@ -12,6 +12,8 @@ export function MeteorPreview() {
 
   return (
     <div className="h-[40rem] flex items-center justify-center">
+      <title>Meteoros</title>
+
       <div className=" h-3/4 md:h-1/2 w-3/4  relative max-w-sm">
         <div className="absolute inset-0 h-full w-full bg-gradient-to-r from-blue-500 to-teal-500 transform scale-[0.80] bg-red-500 rounded-full blur-3xl" />
         <AddMeteor setMeteors={setMeteors} />
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,16 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="alternate icon" href="/favicon.ico" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
